076a6550959c94e8cbb55421a1644e630c8817da
Ritik <ritiksinha005@gmail.com>
2017-12-09 15:37:00 +0530

    check for overlapping but with a bug for now

--- patternOccurence.py
+++ patternOccurence.py
@@ -1,12 +1,16 @@
-import re
 # Input:  Two strings, Pattern and Genome
 # Output: A list containing all starting positions where Pattern appears as a substring of Genome
 def PatternMatching(Pattern, Genome):
     positions = [] # output variable
     pattern = Pattern
     genome = Genome
-    positions.append([m.start() for m in re.finditer(pattern,genome)])
+    start = 0
+    while (start < (len(genome) - len(pattern)) + 1):
+    	position = genome.find(pattern,start)
+    	start = genome.find(pattern,start) + 1
+    	positions.append(position)
     return positions
+
 inp = input("Enter Genome: ")
 pat = input("Enter Pattern: ")
 print(PatternMatching(pat,inp))
